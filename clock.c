90d84483acc0f28e0cb94485d0155d5e87524bd7
ansh67283 <120741334+ansh67283@users.noreply.github.com>
2023-03-16 23:45:57 +0530

    Add files via upload

---
+++ clock.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+int main() {
+    time_t currentTime;
+    struct tm *localTime;
+	while(1) {
+        // Get current time
+        currentTime = time(NULL);
+        localTime = localtime(&currentTime);
+		// Print time in digital format
+        printf("%02d:%02d:%02d\r", localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
+		// Wait for 1 second
+        sleep(1);
+    }
+	return 0;
+}
+
